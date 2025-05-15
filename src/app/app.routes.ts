5790096c41af8e4e5d57f63898f41ba3991b3dfd
alinasser2 <alinasserelden@hotmail.com>
2025-05-15 23:01:58 +0300

    add routing configuration with LayoutComponent and ListShipmentsComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './core/layout/layout.component';
+import { ListShipmentsComponent } from './features/shipments/components/list-shipments/list-shipments.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'shipments',
+        pathMatch: 'full'
+      },
+      {
+        path: 'shipments',
+        component: ListShipmentsComponent
+      }
+    ]
+  }
+];
