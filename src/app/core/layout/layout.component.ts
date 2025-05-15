2c5fe08e6e95ca09ca5e1fbbad693425adb8af1b
alinasser2 <alinasserelden@hotmail.com>
2025-05-16 00:37:06 +0300

    add navigation methods for creating and viewing shipments

--- src/app/core/layout/layout.component.ts
+++ src/app/core/layout/layout.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
@@ -9,5 +10,13 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './layout.component.scss'
 })
 export class LayoutComponent {
+constructor(private router: Router) {}
 
+  navigateToCreate() {
+    this.router.navigate(['/shipments/new']);
+  }
+
+  navigateToShipments() {
+    this.router.navigate(['/shipments']);
+  }
 }
